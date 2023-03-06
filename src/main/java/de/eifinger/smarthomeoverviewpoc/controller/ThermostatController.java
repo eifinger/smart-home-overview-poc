6dfc2eadc0d41cf51c6e2374cc7f9745209619a7
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2023-03-06 16:11:06 +0100

    docs: Add swaggerui documentation for ThermostatController

--- src/main/java/de/eifinger/smarthomeoverviewpoc/controller/ThermostatController.java
+++ src/main/java/de/eifinger/smarthomeoverviewpoc/controller/ThermostatController.java
@@ -2,6 +2,8 @@ package de.eifinger.smarthomeoverviewpoc.controller;
 
 import de.eifinger.smarthomeoverviewpoc.domain.thermostat.Thermostat;
 import de.eifinger.smarthomeoverviewpoc.domain.thermostat.ThermostatRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -18,8 +20,12 @@ public class ThermostatController {
         this.thermostatRepository = thermostatRepository;
     }
 
+    @Operation(summary = "Get all thermostats. Optionally filter by room and/or home.")
     @GetMapping("/thermostat")
-    public Flux<Thermostat> getThermostats(@RequestParam Optional<Long> roomId, @RequestParam Optional<Long> homeId) {
+    public Flux<Thermostat> getThermostats(
+            @Parameter(description = "Filter by room id") @RequestParam Optional<Long> roomId,
+            @Parameter(description = "Filter home id") @RequestParam Optional<Long> homeId
+    ) {
 
         return roomId
                 .map(thermostatRepository::findByAssignedRoom)
@@ -30,21 +36,31 @@ public class ThermostatController {
                 );
     }
 
+    @Operation(summary = "Get an existing thermostat by id")
     @GetMapping("/thermostat/{thermostatId}")
-    public Mono<Thermostat> getThermostat(@PathVariable Long thermostatId) {
+    public Mono<Thermostat> getThermostat(
+            @Parameter(description = "The thermostat id")  @PathVariable Long thermostatId
+    ) {
         return thermostatRepository.findById(thermostatId);
     }
 
+    @Operation(summary = "Create a new thermostat")
     @PutMapping("/thermostat")
     @ResponseStatus(HttpStatus.CREATED)
-    public Mono<Thermostat> createThermostat(@RequestBody String thermostatName) {
+    public Mono<Thermostat> createThermostat(
+            @Parameter(description = "The thermostat name") @RequestBody String thermostatName
+    ) {
         var newThermostat = Thermostat.builder().name(thermostatName).build();
         return thermostatRepository.save(newThermostat);
     }
 
+    @Operation(summary = "Assign an existing thermostat to a room")
     @PostMapping("/thermostat/{thermostatId}/assignRoom")
     @ResponseStatus(HttpStatus.CREATED)
-    public Mono<Thermostat> assignThermostatToRoom(@PathVariable Long thermostatId, @RequestBody Long roomId) {
+    public Mono<Thermostat> assignThermostatToRoom(
+            @Parameter(description = "The thermostat id") @PathVariable Long thermostatId,
+            @Parameter(description = "The room id") @RequestBody Long roomId
+    ) {
         return thermostatRepository.findById(thermostatId)
                 .flatMap(thermostat -> thermostatRepository.save(thermostat.assignToRoom(roomId)));
 
