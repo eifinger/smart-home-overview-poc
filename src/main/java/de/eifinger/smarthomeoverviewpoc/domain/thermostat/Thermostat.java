eb38943a254a5af2224b7466e9e06f37c64dd662
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2023-03-06 11:10:48 +0100

    feat: Generate random temp/hum values

--- src/main/java/de/eifinger/smarthomeoverviewpoc/domain/thermostat/Thermostat.java
+++ src/main/java/de/eifinger/smarthomeoverviewpoc/domain/thermostat/Thermostat.java
@@ -3,11 +3,17 @@ package de.eifinger.smarthomeoverviewpoc.domain.thermostat;
 import lombok.Builder;
 import lombok.Value;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.relational.core.mapping.Column;
+
+import java.util.Random;
 
 @Value
 @Builder
 public class Thermostat {
+
+    @Transient
+    private final Random random = new Random();
 
     @Id
     Long id;
@@ -23,6 +29,14 @@ public class Thermostat {
 
     @Column("assigned_room")
     Long assignedRoom;
+
+    public float getHumidity() {
+        return 10.0f + random.nextFloat() * (95.0f - 10.0f);
+    }
+
+    public float getTemperature() {
+        return 5.0f + random.nextFloat() * (28.0f - 5.0f);
+    }
 
     public Thermostat assignToRoom(Long roomId) {
         return Thermostat.builder()
